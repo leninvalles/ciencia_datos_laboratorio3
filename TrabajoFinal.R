f3daca4d3bfec0cbb5b7d93cd5c321423fa98b67
leninvalles <lvallesrios@hotmail.com>
2023-11-22 06:07:14 -0500

    Actualizaci¢n de Formateo de File epa_http

--- TrabajoFinal.R
+++ TrabajoFinal.R
@@ -26,6 +26,9 @@ class(epa_http$X2)
 #Procedemos a formatear el nombre de las columnas
 colnames(epa_http) <- c("IP", "Tiempo", "Tipo", "URL", "Protocolo", "Codigo", "Bytes")
 
+#Corroboramos la correcta aplicación de los cambios
+class(epa_http$Tiempo)
+
 #Realizamos el formateo el valor "-" por "0"
 epa_http$Bytes <- replace(epa_http$Bytes, epa_http$Bytes == "-", 0)
 
@@ -34,6 +37,21 @@ epa_http$Bytes <- as.numeric(epa_http$Bytes)
 
 #Procedemos al formateo del tipo de valor de la columna Codigo a factor
 epa_http$Codigo <- as.factor(epa_http$Codigo)
+
+#Procederemos a Formatear el tipo de valor de la colunma Tiempo a fecha
+epa_http$Tiempo <- as.POSIXct(epa_http$Tiempo, format = "[%d:%H:%M:%S]",tz="CET")
+
+#
+#
+#2. Identificar el número único de usuarios que han interactuado directamente con
+#el servidor de forma segregada según si los usuarios han tenido algún tipo de
+#error en las distintas peticiones ofrecidas por el servidor..
+#
+#
+
+
+
+
 
 
 
